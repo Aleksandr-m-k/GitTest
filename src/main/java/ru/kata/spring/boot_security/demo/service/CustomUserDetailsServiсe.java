a853aa1ac01dde932f749ae37c6c942f51f3d715
Aleksandr-m-k <a.k.85@mail.ru>
2024-07-29 01:40:35 +0200

    все работает. нужно перепроверить структуру проекта и код

--- src/main/java/ru/kata/spring/boot_security/demo/service/CustomUserDetailsServiсe.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/CustomUserDetailsServiсe.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.repositories.UserRepository;
 
@@ -22,6 +23,7 @@ public class CustomUserDetailsServiсe implements UserDetailsService {
 
 
     @Override
+    @Transactional // РАЗОБРАТЬСЯ С ЭТИМ
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username);
         if (user == null) {
