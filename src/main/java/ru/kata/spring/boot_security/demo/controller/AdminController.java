9c0c3b9910116c2811176f0d111fbf2aec60a07a
Aleksandr-m-k <a.k.85@mail.ru>
2024-08-20 21:36:57 +0200

    №2 не реализована страница логирования и списки ролей  на страницах EDIT и newUser

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -5,10 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.RoleService;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -24,6 +26,7 @@ public class AdminController {
         this.userService = userService;
         this.roleService = roleService;
     }
+
 
     @GetMapping()
     public String getAllUsers(Model model) {
@@ -51,14 +54,26 @@ public class AdminController {
         return "redirect:/admin";
     }
 
-    @GetMapping("/{id}/edit")
+    @GetMapping("/{id}/editUser")
     public String editUser(Model model, @PathVariable("id") int id) {
+        System.out.println("editUser method called___________________________");
+
+        List<Role> rolesList = roleService.findAllRoles();
+        System.out.println("Roles (before check): _________________" + rolesList); // Проверяем результат вызова метода
+
+        if (rolesList == null || rolesList.isEmpty()) {
+            System.out.println("Roles list is null or empty!______________________"); // Сообщение в случае проблемы
+        } else {
+            System.out.println("Roles count: _________________________" + rolesList.size());
+        }
+
         model.addAttribute("user", userService.getUserById(id));
-        model.addAttribute("rolesList", roleService.findAllRoles());
-        return "edit";
+        model.addAttribute("rolesList", rolesList);
+        return "editUser";
     }
 
-    @PostMapping("/{id}")
+
+    @PostMapping("/editUser")
     public String updateUser(@ModelAttribute User user, @RequestParam List<Integer> roleIds) {
         userService.updateUser(user, roleIds);
         return "redirect:/admin";
--- src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -15,7 +15,6 @@ public class Role implements GrantedAuthority {
 
     public Role() {
     }
-
     public Role(int id, String role) {
         this.id = id;
         this.role = role;
